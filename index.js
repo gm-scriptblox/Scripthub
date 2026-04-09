ee2ba68b3b6b6b45ab4162b800d2806b6276f47e
gm-scripts <robloxoiaiiaii212@gmail.com>
2026-04-09 22:43:23 +0800

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,7 +0,0 @@
-<script>
-const path = window.location.pathname.slice(1);
-fetch("https://api.allorigins.win/raw?url=" + encodeURIComponent("https://paste.rs/" + path))
-  .then(r => r.text())
-  .then(t => document.getElementById("content").textContent = t)
-  .catch(e => document.getElementById("content").textContent = "Error fetching paste.");
-</script>
